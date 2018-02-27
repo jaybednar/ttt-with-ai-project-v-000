611283fc16b0db83f3798e45dc7d0eecd53b45ec
Jay Bednar <ironviralmedia@gmail.com>
2018-02-27 00:09:32 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -25,7 +25,7 @@ module Players
         elsif self.game.board.turn_count == 1 && @positions[4] == " "
           @move_ind = 4
         elsif self.game.board.turn_count == 1 && @positions[4] != " "
-          @move_ind = corners.last
+          @move_ind = open_positions.sample
         elsif self.game.board.turn_count == 2
           @move_ind = corners[1]
         elsif find_win
